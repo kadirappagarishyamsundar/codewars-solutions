a9aa121c32d70f9cf9c0a7e1809ac709562a723d
Shyam Sundar <kadirappagarishyamsundar@gmail.com>
2026-07-10 16:40:24 +0530

    solved one 7kyu solution

---
+++ 7kyu/vowel_count.py
@@ -0,0 +1,9 @@
+#problem:Vowel Count
+#https://www.codewars.com/kata/54ff3102c1bad923760001f3
+
+def get_count(sentence):
+    count = 0
+    for char in sentence:
+        if char in 'aeiou':
+            count += 1
+    return count
